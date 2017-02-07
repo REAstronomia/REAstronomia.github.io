b38cd2f024c1470ce8d4b0e6d32e34bc07a0408e
fer8707 <fer8707@gmail.com>
2017-02-07 11:59:45 -0600

    Add files via upload

---
+++ send1.php
@@ -0,0 +1,46 @@
+ <?php 
+
+$name = $_POST['name'];
+$last = $_POST['last']; 
+$nivel = $_POST['nivel'];
+$year = $_POST['year'];
+$instituto = $_POST['instituto'];
+$area = $_POST['area'];
+$mail = $_POST['mail'];
+$titulo = $_POST['titulo']; 
+$resumen = $_POST['abstract'];
+$aop = $_POST['aop'];
+$poster = $_POST['poster'];
+$primera = $_POST['primera'];
+$apoyo = $_POST['apoyo'];
+$Header = 'From: '.$Email ." \r\n";
+$Header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$Header .= "Mime-Version: 1.0 \r\n";
+$Header .= "Content-Type: text/plain";
+
+$Mensaje .= "Nombre :" .$name." \r\n";
+$Mensaje .= "Apellido :" .$last." \r\n";
+$Mensaje .= "Nivel: " .$nivel. " \r\n";
+$Mensaje .= "Año de inicio: ".$year." \r\n";
+$Mensaje .= "Instituto: ".$instituto." \r\n";
+$Mensaje .= "Área: ".$area." \r\n";
+$Mensaje .= "Mail: ".$mail." \r\n";
+$Mensaje .= "Titulo: ".$titulo." \r\n";
+$Mensaje .= "Resumen: ".$resumen." \r\n";
+$Mensaje .= "Estado: ".$aop." \r\n";
+$Mensaje .= "Poster: ".$poster." \r\n";
+$Mensaje .= "Novato: ".$primera." \r\n";
+$Mensaje .= "Apoyo: ".$apoyo." \r\n";
+
+
+$Mensaje .= "Enviado el ".date('d/m/Y', time());
+
+$Para = 'fer8707@gmail.com';
+$Asunto = 'Inscripcio desde Web Campus';
+
+mail($Para, $Asunto, utf8_decode($Mensaje), $Header);
+
+echo "Mensaje enviado correctamente";
+
+
+?> 
